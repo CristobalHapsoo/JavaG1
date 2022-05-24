aaa1333886bcdc86383202eafb5e59ad656f110f
Cristobal Hapsoo Torres <cristobalhapsoo@gmail.com>
2022-05-24 17:36:45 -0400

    Clase23052022

---
+++ JavaG1/src/cl/generetion23052022/Ejercicio1.java
@@ -0,0 +1,27 @@
+package cl.generetion23052022;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double base = 5.5;
+		double altura = 3.2;
+		int constanteTriangulo = 2;
+		//Declaracion variables perimetro circulo
+		double pi = 3.14;
+		double radio = 8;
+		
+		
+		//Variable para operación area de triangulo
+		double areaTriangulo = (base * altura)/constanteTriangulo;
+		//Variable para operación perimetro de circulo
+		double perimetroCirculo = (2 * pi) * radio;
+		//Se imprime area triangulo
+		System.out.println(areaTriangulo);
+		//Se imprime perimetro circulo
+		System.out.println(perimetroCirculo);
+
+
+	}
+
+}
